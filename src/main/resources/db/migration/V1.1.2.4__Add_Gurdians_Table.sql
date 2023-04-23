56c8dd9b98ab86ac30110f9cd67d14c2bb717815
Samuel Butler <sambutler1017@icloud.com>
2023-04-23 11:09:00 -0400

    no message

--- src/main/resources/db/migration/V1.1.2.4__Add_Gurdians_Table.sql
+++ src/main/resources/db/migration/V1.1.2.4__Add_Gurdians_Table.sql
@@ -11,17 +11,18 @@
 -- ---------------------------------------------------------------------------------
 
 CREATE TABLE gurdians (
-  id           INT          unsigned NOT NULL AUTO_INCREMENT,
-  first_name   VARCHAR(128)          NOT NULL,
-  last_name    VARCHAR(128)          NOT NULL,
-  relationship VARCHAR(128)          NOT NULL  DEFAULT 'OTHER'
-  email        VARCHAR(128)                    DEFAULT NULL,
-  phone        VARCHAR(128)          NOT NULL,
-  address      VARCHAR(128)                    DEFAULT NULL,
-  insert_date  DATETIME              NOT NULL  DEFAULT CURRENT_TIMESTAMP(),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  id           INT unsigned NOT NULL AUTO_INCREMENT,
+  first_name   VARCHAR(128) NOT NULL,
+  last_name    VARCHAR(128) NOT NULL,
+  relationship VARCHAR(128) NOT NULL DEFAULT 'OTHER',
+  email        VARCHAR(128)          DEFAULT NULL,
+  phone        VARCHAR(128) NOT NULL,
+  address      VARCHAR(128)          DEFAULT NULL,
+  insert_date  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
 
-CREATE UNIQUE INDEX gurdians_AK1 ON users(email);
+CREATE UNIQUE INDEX gurdians_AK1 ON gurdians(email);
 -- ---------------------------------------------------------------------------------
 -- AWANA-2: END
 -- ---------------------------------------------------------------------------------
