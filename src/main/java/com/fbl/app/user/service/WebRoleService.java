8be53a5cd6e0b4aac33ea471f7f254b67b225ec3
Samuel Butler <sambutler1017@icloud.com>
2023-09-19 13:52:24 -0400

    no message

--- src/main/java/com/fbl/app/user/service/WebRoleService.java
+++ src/main/java/com/fbl/app/user/service/WebRoleService.java
@@ -7,12 +7,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fbl.app.user.client.domain.request.WebRoleGetRequest;
 import com.fbl.common.enums.WebRole;
 import com.fbl.common.page.Page;
 import com.fbl.common.util.CommonUtil;
+import com.fbl.jwt.utility.JwtHolder;
 
 /**
  * Web Role Service class that handles all service calls to the dao
@@ -26,6 +28,9 @@ public class WebRoleService {
     private static final List<WebRole> FILTERED_ROLES = List.of(WebRole.LEADER, WebRole.WORKER, WebRole.USER,
             WebRole.GURDIAN);
 
+    @Autowired
+    private JwtHolder jwtHolder;
+
     /**
      * Gets a list of of web roles based on the request
      * 
@@ -33,8 +38,10 @@ public class WebRoleService {
      * @return list of webroles objects
      */
     public Page<WebRole> getRoles(WebRoleGetRequest request) {
+        WebRole highestUserRole = WebRole.highestRoleRank(jwtHolder.getWebRole());
         List<WebRole> roles = Arrays.asList(WebRole.values()).stream()
                 .filter(r -> !FILTERED_ROLES.contains(r))
+                .filter(r -> r.getRank() <= highestUserRole.getRank())
                 .collect(Collectors.toList());
 
         return CommonUtil.enumListToPage(roles, request);
